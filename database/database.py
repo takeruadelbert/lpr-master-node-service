8ca7d7906c2a6dd17d385ac013ceea1d88ed91f5
Lukas Fam <takeruadelbert@gmail.com>
2020-11-10 11:48:46 +0700

    added cash script to install necessary dependencies.

--- database/database.py
+++ database/database.py
@@ -1,4 +1,3 @@
-import os
 import sqlite3
 
 from misc.constant.value import *
---
+++ install-package.sh
@@ -0,0 +1,3 @@
+virtualenv venv -q -p /usr/bin/python3.8
+source venv/bin/activate
+venv/bin/pip install -r requirements.txt
